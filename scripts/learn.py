e1f94a712cd58df3f34368ad84c0285bfcc021c6
pjain03 <pulkit.jain@tufts.edu>
2018-05-15 00:49:32 -0400

    initialized the learning script

---
+++ scripts/learn.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# Goal: to create a neural network that reads from input stored within project,
+#       trains, validates, and tests on it, and presents the results of this.
+
+# NOTE: we use the techniques outlined in the pictures we include in the README
+#       or under ../src
+
